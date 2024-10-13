3b132545bd608edf1e847ea0e9536170e5c1657d
Chloe <chloeweng@ucsb.edu>
2024-10-13 14:46:21 -0700

    deprecated refs

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -22,5 +22,35 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_correct_bool() {
+        Team other = new Team("test-team");
+        team.addMember("John");
+        team.addMember("Bob");
+        other.addMember("John");
+        other.addMember("Bob");
+        assertEquals(true, team.equals(team));
+        assertEquals(false, team.equals("hi"));
+        assertEquals(true, team.equals(other));
+        other.addMember("Tom");
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void hashCode_returns_correct_number() {
+        Team other = new Team("test-team");
+        team.addMember("John");
+        team.addMember("Bob");
+        other.addMember("John");
+        other.addMember("Bob");
+        assertEquals(team.hashCode(), other.hashCode());
+        int result = team.hashCode();
+        int expectedResult = -1225785413;
+        assertEquals(expectedResult, result);
+    }
 }
